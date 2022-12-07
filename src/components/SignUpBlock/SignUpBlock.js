c7d95cf7ddc7753a8cd6a7dc21ae5191bb046e55
Nikita <nikitazukanov@mail.ru>
2022-12-07 09:11:02 +0300

    add rules for signUp

--- src/components/SignUpBlock/SignUpBlock.js
+++ src/components/SignUpBlock/SignUpBlock.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './SignUpBlock.css';
 import SideButtons from "../SideButtons/SideButtons";
 import SignUpForm from "../SignUpForm/SignUpForm";
@@ -13,31 +13,10 @@ const SignUpBlock = (props) => {
         tel: '',
     })
 
-    function setLogin(e) {
+    function setValue(e, value) {
+        signUpForm[value] = e.target.value
         setSignUpForm({
-          ...signUpForm,
-          login: e.target.value,
-        })
-    }
-    
-    function setPassword(e) {
-        setSignUpForm({
-          ...signUpForm,
-          password: e.target.value,
-        })
-    }
-    
-    function setConfirmPass(e) {
-        setSignUpForm({
-          ...signUpForm,
-          confirmPass: e.target.value,
-        })
-    }
-    
-    function setTel(e) {
-        setSignUpForm({
-          ...signUpForm,
-          tel: e.target.value,
+          ...signUpForm
         })
     }
       
@@ -45,18 +24,110 @@ const SignUpBlock = (props) => {
         console.log('submit')
     }
 
+    const [submitDisabled, setSubmitDisabled] = useState(true)
+
+    useEffect(() => {
+        if (signUpForm.login === '' || signUpForm.password === '' || signUpForm.confirmPass === '' || signUpForm.tel === '') {
+            setSubmitDisabled(true)
+        }
+        else if (signUpForm.login !== '' && signUpForm.password !== '' && signUpForm.confirmPass !== '' && signUpForm.tel !== '') {
+            setSubmitDisabled(false)
+        }
+    }, [signUpForm])
+
+
+    const [passmatch, setPassmatch] = useState('')
+
+    useEffect(() => {
+        if (signUpForm.password !== signUpForm.confirmPass) {
+            setSubmitDisabled(true);
+            setPassmatch('password missmatch');
+        }
+        else if (signUpForm.password === signUpForm.confirmPass && signUpForm.password !== '') {
+            setPassmatch('');
+        }
+    },[signUpForm.confirmPass, signUpForm.password])
+
+    const [passLevel, setPassLevel] = useState({
+        text: '',
+        color: '',
+    })
+
+    useEffect(() => {
+        let simplepass = `012345678909876543210${signUpForm.login}`;
+        let arr = signUpForm.password.split('')
+        let set = new Set(arr)
+        if (arr.length < 4 && arr.length !== 0) {
+            setPassLevel({
+                ...passLevel,
+                text: 'password is too short',
+                color: 'red'
+            })
+            setSubmitDisabled(true)
+        }
+        if (arr.length >= 4 && arr.length !== 0) {
+            setPassLevel({
+                ...passLevel,
+                text: 'simple password',
+                color: 'rgb(191, 95, 0)'
+            })
+        }
+        if (arr.length >= 6 && set.size >= 4 && arr.length !== 0) {
+            setPassLevel({
+                ...passLevel,
+                text: 'normal password',
+                color: 'yellow'
+            })
+        }
+        if (arr.length >= 8 && set.size >= 6 && arr.length !== 0) {
+            setPassLevel({
+                ...passLevel,
+                text: 'good password',
+                color: 'green'
+            })
+        }
+        if (arr.length >= 4 && simplepass.includes(signUpForm.password)) {
+            setPassLevel({
+                ...passLevel,
+                text: 'simple password',
+                color: 'rgb(191, 95, 0)'
+            })
+        }
+
+    },[signUpForm.password, signUpForm.login])
+
+
+    const [incorrectNum, setIncorrectNum] = useState('')
+
+    useEffect(() => {
+        let arr = signUpForm.tel.split('');
+        let correct = '+0123456789';
+        arr.forEach(item => {
+            if (!correct.includes(item)) {
+                setSubmitDisabled(true);
+                setIncorrectNum('incorrect number');
+            }
+            else setIncorrectNum('')
+        })
+        if (arr.length !== 13 && arr.length !== 0) {
+            setSubmitDisabled(true);
+            setIncorrectNum('incorrect number');
+        }
+    },[signUpForm.tel])
+
+
     
     return (
         <div className='signUp'>
             <div className='wrapper'>
                 <SignUpForm 
+                    submitDisabled={submitDisabled}
                     signUpForm={signUpForm}
                     submitSignUp={submitSignUp}
-                    setLogin={setLogin}
-                    setPassword={setPassword}
-                    setConfirmPass={setConfirmPass}
-                    setTel={setTel}
-
+                    setValue={setValue}
+                    passmatch={passmatch}
+                    passLevel={passLevel}
+                    incorrectNum={incorrectNum}
                 />
                 <SideButtons />
             </div>
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -7,14 +7,17 @@ const SignUpForm = (props) => {
         <div className='container'>
             <form className='signUpForm' onSubmit={props.submitSignUp}>
                 <label htmlFor='login'>Login</label>
-                <input className='input' id='login' autoComplete='off' type="text" value={props.signUpForm.login} onChange={(e) => props.setLogin(e)} />
+                <input className='input' id='login' autoComplete='off' type="text" value={props.signUpForm.login} onChange={(e) => props.setValue(e, 'login')} />
                 <label htmlFor='password'>Password</label>
-                <input className='input' id='password' type="password" value={props.signUpForm.password} onChange={(e) => props.setPassword(e)} />
+                <input className='input' id='password' type="password" value={props.signUpForm.password} onChange={(e) => props.setValue(e, 'password')} />
+                <div className="underText" style={{color: props.passLevel.color}}>{props.passLevel.text}</div>
                 <label htmlFor='confirmPass'>Confirm password</label>
-                <input className='input' id='confirmPass' type="password" value={props.signUpForm.confirmPass} onChange={(e) => props.setConfirmPass(e)} />
+                <input className='input' id='confirmPass' type="password" value={props.signUpForm.confirmPass} onChange={(e) => props.setValue(e, 'confirmPass')} />
+                <div className="underText passmatch">{props.passmatch}</div>
                 <label htmlFor='tel'>Telphone number</label>
-                <input className='input' id='tel' type="tel" value={props.signUpForm.tel} onChange={(e) => props.setTel(e)} />
-                <button className='signUpBtn' type='submit'>Sign Up</button>
+                <input className='input' id='tel' type="tel" value={props.signUpForm.tel} onChange={(e) => props.setValue(e, 'tel')} />
+                <div className="underText passmatch">{props.incorrectNum}</div>
+                <button disabled={props.submitDisabled} className='signUpBtn' type='submit'>Sign Up</button>
             </form>
         </div>
     )
